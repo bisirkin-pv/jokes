c9d5e6652d248f5713885be9b48b7507884f6b85
paveldriver <paveldriver0@gmail.com>
2018-09-22 21:57:19 +0300

    Add test on availability api url for receiving id

--- service/tests/test_api.py
+++ service/tests/test_api.py
@@ -8,7 +8,10 @@ class TestApi(unittest.TestCase):
 
     def test_on_availability(self):
         request = self.http.request('GET', 'http://localhost:8787')
-        print(request.status)
+        self.assertEqual(request.status, 200)
+
+    def test_on_availability_url_receiving_id(self):
+        request = self.http.request('GET', 'http://localhost:8787/api/v1/jokes/1')
         self.assertEqual(request.status, 200)
 
     def tearDown(self):
