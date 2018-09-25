818a6e29953a8c206f88ece6b9635ad2334c023b
paveldriver <paveldriver0@gmail.com>
2018-09-25 22:29:33 +0300

    Add simple test execute query

--- service/tests/test_db.py
+++ service/tests/test_db.py
@@ -7,12 +7,17 @@ from service.src.utility.PropertyReader import PropertyReader
 
 class TestDatabase(unittest.TestCase):
     def setUp(self):
-        self.conn = None
-
-    def test_connect(self):
         db = DataBase(PropertyReader("../../dbSettingPostgres.ini"))
         self.conn = db.connect()
+
+    def test_connect(self):
         self.assertIsNotNone(self.conn, 'Error connecting to database')
+
+    def test_request(self):
+        cur = self.conn.cursor()
+        cur.execute('SELECT version()')
+        db_version = cur.fetchone()
+        self.assertIsNotNone(db_version, 'Error execute query')
 
     def tearDown(self):
         if self.conn is not None:
